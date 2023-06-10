fd0aaafb5cc0ec4150a0b198b6aef85bb2202629
TheWhisperer <houstonplanes@gmail.com>
2023-06-10 22:42:33 +0300

    Изменил собственную реализацию тостов на реализацию Blazored.Toasts

---
+++ CafeMaya/Hubs/NotificationHub.cs
@@ -0,0 +1,12 @@
+﻿using CafeMaya.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace CafeMaya.Hubs;
+
+public class NotificationHub : Hub
+{
+    public async Task SendNewOrderNotification(int newOrderId)
+    {
+        await Clients.All.SendAsync("NewOrder", newOrderId);
+    }
+}
--- CafeMaya/Program.cs
+++ CafeMaya/Program.cs
@@ -1,18 +1,26 @@
+using Blazored.Toast;
 using CafeMaya.Models;
 using CafeMaya.Services;
 using CafeMaya.Services.Validators;
 using FluentValidation;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.ResponseCompression;
+using CafeMaya.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 var connStrings = builder.Configuration.GetSection("ConnectionStrings");
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddBlazoredToast();
+builder.Services.AddResponseCompression(opts =>
+{
+    opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[] { "application/octet-stream" });
+});
 builder.Services.AddDbContextFactory<OrderDataProvider>(opt =>
     opt.UseSqlServer(connStrings["Default"]));
 builder.Services.AddTransient<IValidator<Order>, OrderValidator>();
 var app = builder.Build();
-
+app.UseResponseCompression();
 if (!app.Environment.IsDevelopment())
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -26,6 +34,7 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.MapBlazorHub();
+app.MapHub<NotificationHub>("/notificationhub");
 app.MapFallbackToPage("/_Host");
 
 app.Run();
