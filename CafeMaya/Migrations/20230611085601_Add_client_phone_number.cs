5f533f61fb48d8f7a01dc9a99d3f6a1ce8f3ad51
TheWhisperer <houstonplanes@gmail.com>
2023-06-11 12:00:06 +0300

    Добавил номер телефона клиента, валидатор телефона и маска телефона

---
+++ CafeMaya/Migrations/20230611085601_Add_client_phone_number.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CafeMaya.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_client_phone_number : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ClientPhone",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ClientPhone",
+                table: "Orders");
+        }
+    }
+}
--- CafeMaya/Services/Validators/OrderValidator.cs
+++ CafeMaya/Services/Validators/OrderValidator.cs
@@ -8,14 +8,16 @@ public class OrderValidator : AbstractValidator<Order>
     public OrderValidator()
     {
         RuleFor(o => o.Address)
-            .NotEmpty()
-            .WithMessage("Адрес не должен быть пустым.")
-            .MinimumLength(5)
-            .WithMessage("Адрес доставки не может быть меньше 5 символов.")
+            .NotEmpty().WithMessage("Адрес не должен быть пустым.")
+            .MinimumLength(5).WithMessage("Адрес доставки не может быть меньше 5 символов.")
+            .When(o => o.IsForDelivery);
+
+        RuleFor(o => o.ClientPhone)
+            .NotEmpty().WithMessage("Телефон должен быть предоставлен")
+            .Matches("([\\+]?)([0-9]{1,2}\\([0-9]{3}\\)[0-9]{3}\\-[0-9]{2}\\-[0-9]{2})").WithMessage("Телефон должен быть 11 символов (если без знака +) или 12 символов (если со знаком +) и должен состоять из цифр")
             .When(o => o.IsForDelivery);
 
         RuleFor(o => o.Items)
-            .NotEmpty()
-            .WithMessage("Для формирования заказа нужно добавить блюда.");
+            .NotEmpty().WithMessage("Для формирования заказа нужно добавить блюда.");
     }
 }
